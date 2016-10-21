dd139e45363806d825c0486322339b2d94773969
Nicolas Cornu <ncornu@aldebaran.com>
2016-10-21 19:49:10 +0200

    Move downloading of DA in his own function

--- fbtool.c
+++ fbtool.c
@@ -52,56 +52,13 @@ CHIP_DATA* get_chip_data(uint16_t chip)
     return &(chip_tbl[0]);
 }
 
-int main(int argc, char *argv[])
+static int download_download_agent(tty_usb_handle *h, int is_brom, char *auth_path, char *pl_path,
+        char *lk_path, CHIP_DATA *chip)
 {
-    tty_usb_handle *h = NULL;
-    int r, is_brom = 0;
+    int r;
     void *lk=NULL, *sig_lk=NULL;
     size_t len_lk, len_sig_lk;
-    char *auth_path=NULL, *pl_path=NULL, *lk_path=NULL;
-    uint16_t chip_code;
-    CHIP_DATA* chip;
     char lk_sig_path[4096];
-
-    int c;
-    while ((c = getopt(argc, argv, "ha:")) != -1) {
-        switch(c)
-        {
-        case 'h':
-          printf("Usage: %s [-h] [-a auth] preloader lk\n", argv[0]);
-          return 0;
-        case 'a':
-          auth_path = optarg;
-          break;
-        }
-    }
-    if (argv[optind] == NULL || argv[optind+1] == NULL) {
-        printf("Usage: %s [-h] [-a auth] preloader lk\n", argv[0]);
-        return 1;
-    }
-    pl_path = argv[optind];
-    lk_path = argv[optind+1];
-
-    h = tty_usb_open_auto();
-    is_brom = tty_usb_is_target_brom();
-
-    r = start_cmd(h);
-    if(r)
-    {
-        printf("ERROR: start_cmd(%d)\n", r);
-        return 1;
-    }
-    if(is_brom) printf("connect brom\n");
-    else printf("connect preloader\n");
-
-    r = get_hw_code(h, &chip_code);
-    if(r)
-    {
-        printf("ERROR: get_hw_code(%d)\n", r);
-        return 1;
-    }
-    printf("chip:%x\n", chip_code);
-    chip = get_chip_data(chip_code);
 
     if(is_brom)
     {
@@ -205,6 +162,61 @@ int main(int argc, char *argv[])
     if(r)
     {
         printf("ERROR: jump_da(%d)\n", r);
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    tty_usb_handle *h = NULL;
+    int r, is_brom = 0;
+    char *auth_path=NULL, *pl_path=NULL, *lk_path=NULL;
+    uint16_t chip_code;
+    CHIP_DATA* chip;
+
+    int c;
+    while ((c = getopt(argc, argv, "ha:")) != -1) {
+        switch(c)
+        {
+            case 'h':
+                printf("Usage: %s [-h] [-a auth] preloader lk\n", argv[0]);
+                return 0;
+            case 'a':
+                auth_path = optarg;
+                break;
+        }
+    }
+    if (argv[optind] == NULL || argv[optind+1] == NULL) {
+        printf("Usage: %s [-h] [-a auth] preloader lk\n", argv[0]);
+        return 1;
+    }
+    pl_path = argv[optind];
+    lk_path = argv[optind+1];
+
+    h = tty_usb_open_auto();
+    is_brom = tty_usb_is_target_brom();
+
+    r = start_cmd(h);
+    if(r)
+    {
+        printf("ERROR: start_cmd(%d)\n", r);
+        return 1;
+    }
+    if(is_brom) printf("connect brom\n");
+    else printf("connect preloader\n");
+
+    r = get_hw_code(h, &chip_code);
+    if(r)
+    {
+        printf("ERROR: get_hw_code(%d)\n", r);
+        return 1;
+    }
+    printf("chip:%x\n", chip_code);
+    chip = get_chip_data(chip_code);
+
+    if (download_download_agent(h, is_brom, auth_path, pl_path, lk_path, chip) != 0) {
+        printf("Can't send download agent. Abort.\n");
         return 1;
     }
 
